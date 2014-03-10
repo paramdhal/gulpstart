c590cda4e4d1e674265b28bcca4f599b88fe2f9f
Param Dhaliwal <paramdhal@gmail.com>
2014-03-10 21:29:20 +0000

    Moved app files to app folder

--- gulpfile.js
+++ gulpfile.js
@@ -7,18 +7,18 @@ var gulp = require('gulp'),
 
 // Styles
 gulp.task('styles', function () {
-	return gulp.src('styles/scss/*.scss')
+	return gulp.src('app/styles/scss/*.scss')
 		.pipe(plumber())
 		.pipe(sass({
 			style: 'compressed'
 		}))
-		.pipe(gulp.dest('styles/css/'));
+		.pipe(gulp.dest('app/styles/css/'));
 });
 
 gulp.task('watch', function() {
 
 	// Watch .scss files
-	gulp.watch('styles/scss/*.scss', ['styles']);
+	gulp.watch('app/styles/scss/*.scss', ['styles']);
 
 
 
