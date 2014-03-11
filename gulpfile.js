e088a11c3e98404dbe4ba814a6a74ba8c29e6265
Param Dhaliwal <paramdhal@gmail.com>
2014-03-11 23:32:53 +0000

    Added bourbon and using node sass

--- gulpfile.js
+++ gulpfile.js
@@ -3,20 +3,23 @@
 'use strict';
 
 var gulp = require('gulp'),
-	sass = require('gulp-ruby-sass'),
+	sass = require('gulp-sass'),
 	plumber = require('gulp-plumber'),
-	browserSync = require('browser-sync');
+	browserSync = require('browser-sync'),
+	bourbon = require('node-bourbon').includePaths;
 
 // Styles
 gulp.task('styles', function () {
 	return gulp.src('app/styles/scss/*.scss')
 		.pipe(plumber())
 		.pipe(sass({
-			style: 'compressed'
+			outputStyle: 'compressed',
+			includePaths : [bourbon],
+			errLogToConsole: true
 		}))
 		.pipe(gulp.dest('app/styles/css/'));
 });
-
+// Browser sync
 gulp.task('browser-sync', function() {
 	browserSync.init(['app/styles/css/*.css', 'app/*.html'], {
 		server: {
